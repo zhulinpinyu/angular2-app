8733f9a2f257bf2f2efd2f0442e0aa057d025928
zhulinpinyu <mulixiang199016@gmail.com>
2016-03-16 11:24:54 +0800

    more properties

--- dev/my-component.component.ts
+++ dev/my-component.component.ts
@@ -4,10 +4,13 @@ import {Component} from "angular2/core"
   selector: "my-component",
   template: `
     <h3>
-    Hi {{name}},this my custom Component.<span [class.is-awesome]="input.value==='Yes'">It is so Awesome.</span>
+    Hi <span [style.color]="input.value ==='yes' ? 'red' : ''">{{name}}</span>,this my custom Component.<span [class.is-awesome]="input.value==='yes'">It is so Awesome.</span>
     </h3>
     Is it Awesome?
     <input type="text" #input (keyup)="0"/>
+    <br>
+    <br>
+    <button [disabled]="input.value !== 'yes'">Enable if type 'yes'</button>
   `,
   styleUrls: ["src/css/my-component.css"]
 })
