bc906c67911bae3b3afd77fa3707f252ccb965db
zhulinpinyu <mulixiang199016@gmail.com>
2016-03-16 15:08:00 +0800

    first demo component done

--- dev/puzzle/puzzle.component.ts
+++ dev/puzzle/puzzle.component.ts
@@ -1,4 +1,4 @@
-import {Component} from 'angular2/core'
+import {Component,OnInit} from 'angular2/core'
 
 @Component({
   selector: 'my-puzzle',
@@ -6,24 +6,46 @@ import {Component} from 'angular2/core'
     <section class="setup">
       <h2>Game Setup</h2>
       Enter your name please:
-      <input type="text"/>
+      <input type="text" #name (keyup)="0"/>
     </section>
-    <section class="puzzle">
-      <h2>The Puzzle</h2>
-      <p>ok, welcome <span class="name">XXX</span></p>
+    <section
+      [ngStyle]="{
+        display: name.value === '' ? 'none' : 'block'
+      }"
+      [ngClass]="{
+        puzzle: true,
+        solved: switch1.value == switch1Number && switch2.value == switch2Number && switch3.value == switch3Number && switch4.value == switch4Number
+      }"
+    >
+      <h2>The Puzzle | {{switch1.value == switch1Number && switch2.value == switch2Number && switch3.value == switch3Number && switch4.value == switch4Number ? 'SOLVED' : 'NOT SOLVED'}}
+      </h2>
+      <p>ok, welcome <span class="name">{{name.value}}</span></p>
       <br />
       Switch 1:
-      <input type="text" /><br />
+      <input type="text" #switch1 (keyup)="0"/><br />
       Switch 2:
-      <input type="text" /><br />
+      <input type="text" #switch2 (keyup)="0"/><br />
       Switch 3:
-      <input type="text" /><br />
+      <input type="text" #switch3 (keyup)="0"/><br />
       Switch 4:
-      <input type="text" /><br />
-      <h3>Congratulations XXX,you do it.</h3>
+      <input type="text" #switch4 (keyup)="0"/><br />
     </section>
+    <h2 [hidden]="switch1.value != switch1Number || switch2.value != switch2Number || switch3.value != switch3Number || switch4.value != switch4Number">
+    Congratulations {{name.value}}, you do it.
+    </h2>
   `
 })
-export class PuzzleComponent{
+export class PuzzleComponent implements OnInit{
+  switch1Number:number
+  switch2Number:number
+  switch3Number:number
+  switch4Number:number
 
+  ngOnInit(){
+    this.switch1Number = Math.round(Math.random())
+    this.switch2Number = Math.round(Math.random())
+    this.switch3Number = Math.round(Math.random())
+    this.switch4Number = Math.round(Math.random())
+    console.log(this.switch1Number,this.switch2Number,this.switch3Number,this.switch4Number)
+  }
 }
