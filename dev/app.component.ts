ff6cda853cec969ccabc9b8c00886fdadbbc3f82
zhulinpinyu <mulixiang199016@gmail.com>
2016-03-16 16:46:08 +0800

    basic html structure

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -3,7 +3,12 @@ import {Component} from 'angular2/core';
 @Component({
     selector: 'app',
     template: `
-      
+      <section class="parent">
+        <h2>This is parent component</h2>
+        <section class="child">
+          <h3>This is child component</h3>
+        </section>
+      </section>
     `,
 })
 export class AppComponent {
