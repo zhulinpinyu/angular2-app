a67fa3232bf0ab25d784f1982ead3299ec560ee1
zhulinpinyu <mulixiang199016@gmail.com>
2016-03-16 16:28:43 +0800

    clean code

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,14 +1,10 @@
 import {Component} from 'angular2/core';
-import {MyComponentComponent} from './my-component.component'
 
 @Component({
-    selector: 'my-app',
+    selector: 'app',
     template: `
-        <h1>Angular 2 Boilerplate</h1>
-        <p>Hello World!</p>
-        <my-component></my-component>
+      
     `,
-    directives: [MyComponentComponent]
 })
 export class AppComponent {
 
--- dev/test.component.ts
+++
@@ -1,8 +0,0 @@
-import {Component} from 'angular2/core'
-
-@Component({
-  selector: 'test',
-  templateUrl: 'templates/test.tpl.html'
-})
-export class TestComponent{
-}
