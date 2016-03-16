b8974e42263297baf7b6dffa564751af9a1ab482
zhulinpinyu <mulixiang199016@gmail.com>
2016-03-16 16:57:55 +0800

    alias property name and use other way through value

--- dev/property-binding.component.ts
+++ dev/property-binding.component.ts
@@ -1,14 +1,14 @@
-import {Component} from 'angular2/core'
+import {Component,Input} from 'angular2/core'
 
 @Component({
   selector: 'my-property-binding',
   template: `
     <h3>This is child component</h3>
-    Hi {{myName}}, I am {{myAge}} years old.
+    Hi {{myName}}, I am {{age}} years old.
   `,
-  inputs: ['myName','myAge']
+  inputs: ['myName']
 })
 export class PropertyBindingComponent{
   myName = ''
-  myAge = 0
+  @Input('myAge') age = 10
 }
