ce5047ea1854a8f34d1b9afb4a113a2fd58456c8
zhulinpinyu <mulixiang199016@gmail.com>
2016-03-18 09:22:06 +0800

    每个component 单独使用注入的service示例

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,12 +1,16 @@
 import {Component} from 'angular2/core';
 import {Component1Component} from './component1.component'
+import {Component2Component} from './component2.component'
 
 @Component({
     selector: 'app',
     template: `
+      <h1>First Component</h1>
       <component-1></component-1>
+      <h1>Second Component</h1>
+      <component-2></component-2>
     `,
-    directives: [Component1Component]
+    directives: [Component1Component, Component2Component]
 })
 export class AppComponent{
   
--- dev/services/logging.service.ts
+++ dev/services/logging.service.ts
@@ -2,7 +2,10 @@ import {Injectable} from 'angular2/core'
 
 @Injectable()
 export class LoggingService{
+    private _lastMessage:string
+
     log(message:string){
-        console.log(message)
+        console.log("Current: "+message+" Last:" + this._lastMessage)
+        this._lastMessage = message
     }
 }
