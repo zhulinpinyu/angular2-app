40871fca462c37a150d057e43f6baac039ae6ad3
zhulinpinyu <mulixiang199016@gmail.com>
2016-03-18 17:02:35 +0800

    form prj demo

--- dev/shopping-list/shopping-list-new-item.component.ts
+++ dev/shopping-list/shopping-list-new-item.component.ts
@@ -1,27 +1,48 @@
-import {Component} from 'angular2/core'
+import {Component,OnInit} from 'angular2/core'
+import {ControlGroup, FormBuilder, Validators, Control} from 'angular2/common'
 import {ListItem} from '../list-item'
 import {ShoppingListService} from './shopping-list.service'
 
 @Component({
     selector: "shopping-list-new-item",
     template: `
-        <div class="input">
-            <label for="item-name">Name</label>
-            <input type="text" id="item-name" [(ngModel)]="item.name"/>
-        </div>
-        <div class="input">
-            <label for="item-amt">Amount</label>
-            <input type="text" id="item-amt" [(ngModel)]="item.amount"/>
-        </div>
-        <button (click)="onClick()">Add Item</button>
+        <form [ngFormModel]="myForm" (ngSubmit)="onsubmit()">
+            <div class="input">
+                <label for="item-name">Name</label>
+                <input type="text" id="item-name" [(ngModel)]="item.name" [ngFormControl]="myForm.controls['itemName']"/>
+            </div>
+            <div class="input">
+                <label for="item-amt">Amount</label>
+                <input type="text" id="item-amt" [(ngModel)]="item.amount" [ngFormControl]="myForm.controls['itemAmount']"/>
+            </div>
+            <button type="submit" [disabled]="!myForm.valid" >Add Item</button>
+        </form>
     `
 })
-export class ShoppingListNewItemComponent{
+export class ShoppingListNewItemComponent implements OnInit{
     item = {name: "",amount: 0}
-    constructor(private _shoppingListService:ShoppingListService){}
+    myForm: ControlGroup
 
-    onClick(){
+    constructor(private _shoppingListService:ShoppingListService, private _formBuilder: FormBuilder){}
+
+    onsubmit(){
         this._shoppingListService.insertItem({name: this.item.name,amount: this.item.amount})
         this.item = { name: "", amount: 0 }
+    }
+
+    ngOnInit(){
+        this.myForm = this._formBuilder.group({
+            "itemName": ['',Validators.required],
+            "itemAmount": ['', Validators.compose([
+                Validators.required,
+                greaterZero
+            ])]
+        });
+    }
+}
+
+function greaterZero(control: Control): {[s: string]: boolean}{
+    if(control.value <=0){
+        return {"notEnough": true}
     }
 }
