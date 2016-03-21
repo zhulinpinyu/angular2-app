684da21ce86ec49e34b8fc4800f2b62bacaabff6
zhulinpinyu <mulixiang199016@gmail.com>
2016-03-21 19:22:46 +0800

    from one componet routing to another component

--- dev/lucy.component.ts
+++ dev/lucy.component.ts
@@ -1,11 +1,17 @@
 import {Component} from 'angular2/core'
+import {Router} from 'angular2/router'
 
 @Component({
     selector: 'lucy',
     template: `
         <h3>Hi I am Lucy.</h3>
+        <button (click)="onNav()">Go to Tom</button>
     `
 })
 export class LucyComponent{
+    constructor(private _router: Router){}
 
+    onNav(){
+        this._router.navigate(['Tom'])
+    }
 }
