3355745431268febba2cc85db688575808cbf3d8
zhulinpinyu <mulixiang199016@gmail.com>
2016-03-26 10:32:20 +0800

    add recipe component

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,11 +1,22 @@
-import {Component} from 'angular2/core';
+import {Component} from 'angular2/core'
+import {RecipesComponent} from './recipe-book/recipes.component'
 
 @Component({
     selector: 'my-app',
     template: `
-        <h1>Angular 2 Boilerplate</h1>
-        <p>Hello World!</p>
+        <header>
+            <nav>
+                <ul>
+                    <li><a>Recipes</a></li>
+                    <li><a>Shoping List</a></li>
+                </ul>
+            </nav>
+        </header>
+        <div class="main">
+            <my-recipes></my-recipes>
+        </div>
     `,
+    directives: [RecipesComponent]
 })
 export class AppComponent {
 
---
+++ dev/recipe-book/recipes.component.ts
@@ -0,0 +1,16 @@
+import {Component} from 'angular2/core'
+
+@Component({
+    selector: 'my-recipes',
+    template: `
+        <div class="master list">
+            Recipe Master
+        </div>
+        <div class="detail">
+            Recipe Detail
+        </div>
+    `
+})
+export class RecipesComponent{
+
+}
